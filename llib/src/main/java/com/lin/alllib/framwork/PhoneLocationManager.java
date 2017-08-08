444f1fc1fab9bb0abbc159a146b1b319a6c385e7
linhui <536451207@qq.com>
2017-08-08 10:46:11 +0800

    换了新的 retrofit okhttp rxandroid

---
+++ llib/src/main/java/com/lin/alllib/framwork/PhoneLocationManager.java
@@ -0,0 +1,41 @@
+package com.lin.alllib.framwork;
+
+import com.lin.alllib.data.respone.CityRespone;
+import com.lin.alllib.framwork.commander.IRequestInterceptor;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import okhttp3.Interceptor;
+
+/**
+ * Created by linhui on 2017/8/8.
+ */
+public class PhoneLocationManager implements IRequestInterceptor {
+
+    private static final String TAG ="liv_LocationManager";
+
+    private static PhoneLocationManager phoneLocationManager;
+    {
+        phoneLocationManager = new PhoneLocationManager();
+    }
+    public static final PhoneLocationManager getInstance(){
+        return phoneLocationManager;
+    }
+
+    public PhoneLocationManager(){
+        RequestManager.getInstance().addRequestInterceptor(this);
+    }
+
+    @Override
+    public void onRequest(Interceptor.Chain request) {
+        DebugGod.i(TAG,request.toString());
+    }
+
+    @Override
+    public boolean onResponse(Object o) {
+        DebugGod.i(TAG,o.toString());
+        return false;
+    }
+}
